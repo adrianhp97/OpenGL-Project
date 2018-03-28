02eeaacc6df709c2a0f5d30c1b1b402ead74d99e
Martin Lutta Putra <suka_berkhayal@yahoo.co.id>
2018-03-28 09:30:03 +0700

    Tried wheel

--- Tugas2/Shader/main.cpp
+++ Tugas2/Shader/main.cpp
@@ -8,44 +8,83 @@ using namespace glm;
 
 const int radius = 5;
 GLfloat twicePi = 2.0f * M_PI;
-/*
+static GLfloat backWheel[60];
+static GLfloat frontWheel[60];
+
 void drawFrontWheel() {
-	static GLFloat frontWheel[];
-	float x = 15.0;
+	float x = -20.0;
 	float y = 0;
 	int triangleAmount = 20;
-	for(int i = 0; i < triangleAmount*3; i++) {
-		switch(i%3) {
+	int vertexNum = 0;
+	for(int i = 0; i < triangleAmount*3; i+=3) {
+		switch(vertexNum%3) {
 			case 0:
-				frontWheel[i] = (
-									x + (radius * cos(i * twicePi / triangleAmount)),
-								 	y + (radius * sin(i * twicePi / triangleAmount)), 0.0);
+				frontWheel[i] = x + (radius * cos(i * twicePi / triangleAmount));
+				frontWheel[i+1] = y + (radius * sin(i * twicePi / triangleAmount));
+				frontWheel[i+2] = 0.0;
 				break;
 			case 1:
-				frontWheel[i] = (x, y, 0.0);
+				frontWheel[i] = x;
+				frontWheel[i+1] = y;
+				frontWheel[i+2] = 0.0;
 				break;
 			case 2:
 				if(i == triangleAmount*3 - 1) {
-					frontWheel[i] = (
-										x + (radius * cos(0)),
-									 	y + (radius * sin(0)), 0.0);
+					frontWheel[i] = x + (radius * cos(0));
+					frontWheel[i+1] = y + (radius * sin(0));
+					frontWheel[i+2] = 0.0;
 					
 				} else {
-					frontWheel[i] = (
-									x + (radius * cos(i+1 * twicePi / triangleAmount)),
-								 	y + (radius * sin(i+1 * twicePi / triangleAmount)), 0.0);
+					frontWheel[i] = x + (radius * cos(i+1 * twicePi / triangleAmount));
+					frontWheel[i+1] = y + (radius * sin(i+1 * twicePi / triangleAmount));
+					frontWheel[i+2] = 0.0;
 					
 				} break;
 		}
-		
+		vertexNum++;
 	}
-
+	for(int i = 0; i < triangleAmount*3; i++) {
+		frontWheel[i] /= 30.0;
+	}
 }
 
 void drawBackWheel() {
-	static GLFLoat backWheel[];
+	float x = 15.0;
+	float y = 0;
+	int triangleAmount = 20;
+	int vertexNum = 0;
+	for(int i = 0; i < triangleAmount*3; i+=3) {
+		switch(vertexNum%3) {
+			case 0:
+				backWheel[i] = x + (radius * cos(i * twicePi / triangleAmount));
+				backWheel[i+1] = y + (radius * sin(i * twicePi / triangleAmount));
+				backWheel[i+2] = 0.0;
+				break;
+			case 1:
+				backWheel[i] = x;
+				backWheel[i+1] = y;
+				backWheel[i+2] = 0.0;
+				break;
+			case 2:
+				if(i == triangleAmount*3 - 1) {
+					backWheel[i] = x + (radius * cos(0));
+					backWheel[i+1] = y + (radius * sin(0));
+					backWheel[i+2] = 0.0;
+					
+				} else {
+					backWheel[i] = x + (radius * cos(i+1 * twicePi / triangleAmount));
+					backWheel[i+1] = y + (radius * sin(i+1 * twicePi / triangleAmount));
+					backWheel[i+2] = 0.0;
+					
+				} break;
+		}
+		vertexNum++;
+	}
+	for(int i = 0; i < triangleAmount*3; i++) {
+		backWheel[i] /= 30.0;
+	}
 }
-*/
+
 int main(){
 	//credits to http://www.opengl-tutorial.org
 	if( !glfwInit() ) {
@@ -84,6 +123,9 @@ int main(){
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
+	drawFrontWheel();
+	drawBackWheel();
+
 	static GLfloat g_vertex_buffer_data[] = {
 	   -19.0f, 0.0f, 0.0f,
 	   -27.0f, 1.0f, 0.0f,
@@ -119,6 +161,23 @@ int main(){
 	}
 
 	// This will identify our vertex buffer
+	GLuint vertexbuffer2;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer2);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(frontWheel), frontWheel, GL_STATIC_DRAW);
+
+	GLuint vertexbuffer3;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer3);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer3);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(backWheel), backWheel, GL_STATIC_DRAW);
+
+	// This will identify our vertex buffer
 	GLuint vertexbuffer;
 	// Generate 1 buffer, put the resulting identifier in vertexbuffer
 	glGenBuffers(1, &vertexbuffer);
@@ -127,6 +186,7 @@ int main(){
 	// Give our vertices to OpenGL.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+
 	GLuint programID = LoadShaders("vertexShader.glsl", "fragmentShader.glsl");
 
 	do{
@@ -134,9 +194,37 @@ int main(){
 		// Clear the screen
 		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 		glUseProgram(programID);
-
-	    // 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
+
+
+
+		// 1rst attribute buffer : vertices
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
+		glVertexAttribPointer(
+		   0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		   3,                  // size
+		   GL_FLOAT,           // type
+		   GL_FALSE,           // normalized?
+		   0,                  // stride
+		   (void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 60); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		
+
+		// 1rst attribute buffer : vertices
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer3);
+		glVertexAttribPointer(
+		   0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		   3,                  // size
+		   GL_FLOAT,           // type
+		   GL_FALSE,           // normalized?
+		   0,                  // stride
+		   (void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 60); // Starting from vertex 0; 3 vertices total -> 1 triangle
+
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
 		   0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
@@ -148,6 +236,7 @@ int main(){
 		);
 		// Draw the triangle !
 		glDrawArrays(GL_TRIANGLES, 0, 21); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		
 		glDisableVertexAttribArray(0);
 
 	    // Swap buffers
